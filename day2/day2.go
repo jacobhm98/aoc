4672838e7c9b25927d25df5196826947bfa9b439
jacobhm98 <jacobhm98@gmail.com>
2022-12-07 16:12:59 +0100

    day2

--- day2/day2.go
+++ day2/day2.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	inputLines := readFile("day2/input.txt")
-	playsToPoints := createPlayCombinationToPointsMap()
+	playsToPoints := createPlayCombinationToPointsMapV2()
 	points := calculateTotalPoints(inputLines, playsToPoints)
 	fmt.Println(points)
 }
@@ -37,6 +37,13 @@ func getMovesFromLine(line string) (string, string) {
 //scissor -- p1 C p2 Z
 func createPlayCombinationToPointsMap() map[string]map[string]int {
 	return map[string]map[string]int{"A": {"X": 4, "Y": 8, "Z": 3}, "B": {"X": 1, "Y": 5, "Z": 9}, "C": {"X": 7, "Y": 2, "Z": 6}}
+}
+
+// A -- p1 rock X -- lose p2 -- rock 1
+// B -- p1 paper Y -- draw p2 paper -- 2
+// C -- p1 scissor Z -- win p2 scissor -- 3
+func createPlayCombinationToPointsMapV2() map[string]map[string]int {
+	return map[string]map[string]int{"A": {"X": 0 + 3, "Y": 3 + 1, "Z": 6 + 2}, "B": {"X": 0 + 1, "Y": 3 + 2, "Z": 6 + 3}, "C": {"X": 0 + 2, "Y": 3 + 3, "Z": 6 + 1}}
 }
 
 func readFile(fileName string) []string {
